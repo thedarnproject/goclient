fe80e1fe8ccbd725f630e1916ee500ac6e24ae95
Shubham Minglani <shubham@linux.com>
2017-08-16 22:00:25 +0530

    add logging messages

--- client/main.go
+++ client/main.go
@@ -1,36 +1,39 @@
 package main
 
 import (
-	api "github.com/thedarnproject/thedarnapi/api"
-
 	"flag"
 	"log"
 
-	"google.golang.org/grpc"
 	"context"
-	"fmt"
+
+	"github.com/Sirupsen/logrus"
+	api "github.com/thedarnproject/thedarnapi/api"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	apiAddress := flag.String("api", "localhost:8080", "address of gRPC API")
+	apiAddress := flag.String("api", "localhost:8081", "address of gRPC API")
+	logrus.Infof("connecting to server: %v", *apiAddress)
 	clientConnection, err := grpc.Dial(*apiAddress, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("could not connect to %v: %v", *apiAddress, err)
+		logrus.Fatalf("could not connect to %v: %v", *apiAddress, err)
 	}
 	defer clientConnection.Close()
 
 	client := api.NewErrorInClient(clientConnection)
 
 	clientData := &api.Data{
-		Error: "this is the error",
+		Error:    "this is the error",
 		Platform: "linux",
-		Plugin: "go client",
+		Plugin:   "go client",
 	}
 
+	logrus.Infof("sending data to server:\n%#v", *clientData)
 	success, err := client.Submit(context.Background(), clientData)
 	if err != nil {
 		log.Fatalf("could not submit data to api server %v: %v", *clientData, err)
 	}
-	fmt.Println(success.Success)
-
+	if success.Success {
+		logrus.Info("server returned success")
+	}
 }
